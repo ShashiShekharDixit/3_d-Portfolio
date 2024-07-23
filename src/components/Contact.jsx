96e211a5624f91d15afe230d17880a4ab5b43e0b
Shashi Shekhar Dixit <shashiswadh20@gmail.com>
2024-07-23 23:08:11 +0530

    Correction

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -118,7 +118,7 @@ const Contact = () => {
             type='submit'
             className='bg-tertiary py-3 px-8 rounded-xl outline-none w-fit text-white font-bold shadow-md shadow-primary'
           >
-            {loading ? "Sending..." : "Send"}
+            {loading ? "Sending...." : "Send"}
           </button>
         </form>
       </motion.div>
